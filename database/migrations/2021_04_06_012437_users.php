2c19e47c244c627a3d7496976629783f37d98797
VanNguyen-Kumo <thivan.nguyen@staff.gumiviet.com>
2021-04-06 11:41:33 +0700

    init database

---
+++ database/migrations/2021_04_06_012437_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('id', 36);
+            $table->string('email')->unique();;
+            $table->string('security_code');
+            $table->string('video_type');
+            $table->string('token_key');
+            $table->softDeletesTz();
+            $table->timestamps();
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
--- database/migrations/2021_04_06_021933_failed_jobs.php
+++ database/migrations/2021_04_06_021933_failed_jobs.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class FailedJobs extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,15 @@ class CreateFailedJobsTable extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('id');
             $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
             $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->primary('id');
+            $table->timestampTz('failed_at');
+
         });
     }
 
